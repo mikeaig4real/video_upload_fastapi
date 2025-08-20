7c0bd2a4cfa45b22f1a0725a56b7cbd9ecdf796a
Michael Aigbovbiosa <mikeaig4realonreal@gmail.com>
2025-08-20 22:33:26 +0100

    refactor: Simplify user CRUD operations by removing ID parameter and using current user context

--- app/user/router.py
+++ app/user/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.auth.deps import RequireCurrentUser
-from app.models.id import IDType
 from app.models.success import SuccessModel
 from app.responses import SuccessResponse
 from app.user.crud import user_crud, UserUpdate, UserPublic
@@ -9,25 +8,21 @@ from app.db.deps import RequireSession
 router = APIRouter(prefix="/user")
 
 
-@router.get("/{id}", response_model=SuccessModel[UserPublic])
-async def get(id: IDType, session: RequireSession, current_user: RequireCurrentUser):
-    user = await user_crud.get(
-        id=id, session=session # type: ignore
-    )
+@router.get("/", response_model=SuccessModel[UserPublic])
+async def get(session: RequireSession, current_user: RequireCurrentUser):
+    user = await user_crud.get(id=current_user.id, session=session)  # type: ignore
     return SuccessResponse(user)
 
 
-@router.patch("/{id}", response_model=SuccessModel[UserPublic])
-async def update(id: IDType, update: UserUpdate, session: RequireSession, current_user: RequireCurrentUser):
+@router.patch("/", response_model=SuccessModel[UserPublic])
+async def update(update: UserUpdate, session: RequireSession, current_user: RequireCurrentUser):
     user = await user_crud.update(
-        id=id, data=update, session=session # type: ignore
+        id=current_user.id, data=update, session=session  # type: ignore
     )
     return SuccessResponse(user)
 
 
-@router.delete("/{id}", response_model=SuccessModel[UserPublic])
-async def delete(id: IDType, session: RequireSession, current_user: RequireCurrentUser):
-    user = await user_crud.delete(
-        id=id, session=session # type: ignore
-    )
+@router.delete("/", response_model=SuccessModel[UserPublic])
+async def delete(session: RequireSession, current_user: RequireCurrentUser):
+    user = await user_crud.delete(id=current_user.id, session=session)  # type: ignore
     return SuccessResponse(user)
