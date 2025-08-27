a4e8dc00abda678e6e6455a8b8171d7cf2828a5c
Michael Aigbovbiosa <mikeaig4realonreal@gmail.com>
2025-08-28 00:02:20 +0100

    Refine TODO comments in get_params function to clarify video upload validation and background task scheduling

--- app/upload/router.py
+++ app/upload/router.py
@@ -20,11 +20,11 @@ async def get_params(
     upload: UploadType, _: RequireSession, current_user: RequireCurrentUser
 ):
     """TODO:
-    1. Check if video with the same hash already exists
-    2. If yes, raise an error on footprint already existing
-    3. If no, persist some info about the video in database (adjust video models as needed)
-    4. Create a Celery job that will check all uploads have completed in status within time frame,
-    or else destroy the video's orphaned uploads
+    1. Save some residue data incase the process is not completed properly
+    2. Schedule a background task in advance with retries, use upload_status to check if upload was complete
+    if true, kill task
+    else, use asset id to grab info from bucket, and persist it, 
+    else if data did not even get to bucket, delete existing residue data, kill task too
     """
     # video = video_crud.find_by_hash(hash=upload.upload_hash)
     asset_id = f"{upload.folder}/{current_user.id}/{upload.title}"
