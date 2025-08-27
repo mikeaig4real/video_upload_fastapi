73486d829c3a590a9fbedf55763c33988f240f49
Michael Aigbovbiosa <mikeaig4realonreal@gmail.com>
2025-08-28 00:04:51 +0100

    Refine TODO comments in get_params function to include hash existence check and clarify background task scheduling

--- app/upload/router.py
+++ app/upload/router.py
@@ -20,13 +20,14 @@ async def get_params(
     upload: UploadType, _: RequireSession, current_user: RequireCurrentUser
 ):
     """TODO:
-    1. Save some residue data incase the process is not completed properly
-    2. Schedule a background task in advance with retries, use upload_status to check if upload was complete
+    1. check if hash already exists in db too?
+    # video = video_crud.find_by_hash(hash=upload.upload_hash)
+    2. Save some residue data incase the process is not completed properly
+    3. Schedule a background task in advance with retries, use upload_status to check if upload was complete
     if true, kill task
     else, use asset id to grab info from bucket, and persist it, 
     else if data did not even get to bucket, delete existing residue data, kill task too
     """
-    # video = video_crud.find_by_hash(hash=upload.upload_hash)
     asset_id = f"{upload.folder}/{current_user.id}/{upload.title}"
     params = uploader_crud.generate_params(asset_id=asset_id, resource_type="video")
     return SuccessResponse(params)
